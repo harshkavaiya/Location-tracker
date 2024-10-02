bbd2465817f3b6ef7a03bf950633132f4bc09596
Harsh412589 <hkavaiya001@gmail.com>
2024-10-02 14:04:34 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 
 const http = require("http");
 const socketio = require("socket.io");
+
+const PORT = process.env.PORT || 3000;
 
 // Create server
 const server = http.createServer(app);
@@ -33,6 +36,6 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-server.listen(3000, "0.0.0.0", () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log("Server is running on port 3000");
 });
--- public/js/script.js
+++ public/js/script.js
@@ -22,7 +22,7 @@ if (navigator.geolocation) {
 const map = L.map("map").setView([0, 0], 16);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "harsh kavaiya",
+  attribution: "harsh",
 }).addTo(map);
 
 // Custom icon definition
